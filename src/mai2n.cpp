4173f0b054113be1d0f3ce9e07f140cca01a7d2c
asdf <asdf@ubuntu.ubuntu-domain>
2014-04-05 00:06:30 -0500

    changes

--- src/mai2n.cpp
+++ src/mai2n.cpp
@@ -2079,7 +2079,7 @@ bool LoadBlockIndex(bool fAllowNew)
         }
 
         block.print();
-
+        assert(block.GetHash() == hashGenesisBlock);
 
         // Start new block file
         unsigned int nFile;
